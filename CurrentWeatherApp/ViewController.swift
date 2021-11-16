783fac0f3b8253dd5d53e6851fbc61f2fab9de89
Sinan Kulen <sinan.kulen@gmail.com>
2021-11-16 10:00:37 +0300

    Initial Commit

---
+++ CurrentWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrentWeatherApp
+//
+//  Created by Sinan Kulen on 16.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
